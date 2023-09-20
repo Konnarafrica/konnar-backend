7ecd374c3fb38bf2ce83bb73f3bd1ace55a4abf4
Eneh Kelvin Chukwuemeka <kelvineneh4@gmail.com>
2023-09-20 07:56:53 +0100

    finished the feedback route

--- controllers/feedbackController.js
+++ controllers/feedbackController.js
@@ -2,22 +2,33 @@ import feedbackModel from "../models/feedbackModel.js";
 import userModel from "../models/userModel.js";
 
 export const addFeedback = async (req, res) => {
-    const id = req.params.id;
-    const { feedback_message } = req.body;
-    
-    // const newFeedback = new feedbackModel({
-    //     userId: id,
-    //     feedback_message
-    // });
+  const id = req.params.id;
+  const { feedback_message } = req.body;
 
-    try {
-        const user_with_feedback = await userModel.findById( id );
-        console.log(user_with_feedback)
-        // await newFeedback.save();
-        res.status(200).json({message: "Thanks for your feedback"})
-    } catch (error) {
-        res.status(500).json({error: error.message})
+  if (id === null)
+    return res
+      .status(403)
+      .json({
+        error:
+          "sorry you cannot cannot perform this action because it looks like you are not logged in...",
+      });
+
+  try {
+    const user_with_feedback = await userModel.findById(id);
+    if (user_with_feedback) {
+      const newFeedback = new feedbackModel({
+        userId: id,
+        feedback_message,
+      });
+
+      await newFeedback.save();
+      res.status(200).json({ message: "Thanks for your feedback" });
+    } else {
+      res.status(404).json({ error: "User does not exist..." });
     }
-    
-    console.log(feedback_message)
-}
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+
+  console.log(feedback_message);
+};
