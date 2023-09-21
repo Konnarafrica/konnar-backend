bf86a022c096492c062bc3d78bb3274ff4c06d3a
Eneh Kelvin Chukwuemeka <kelvineneh4@gmail.com>
2023-09-21 22:44:14 +0100

    merge conflict resolved

--- models/agentModel.js
+++ models/agentModel.js
@@ -1,42 +1,36 @@
 import mongoose from "mongoose";
 
-
 const agentSchema = mongoose.Schema(
-    {
-        full_name: { type: String, required: true },
-        bio: { type: String, required: true },
-        email: { type: String, required: true },
-        phone_number: {type: String, required: true},
-        picture: {type: String},
-        availability: [
-            { 
-                status: {type:Boolean, default: false},
-                date: String,
-                time: [String]
-            }
-        ],
-        no_of_properties: {type: Number},
-        area_of_operation: {type: [String]},
-        reviews: [
-            { 
-                id: String,
-                review_msg: String,
-                name_of_user: String
-            }
-        ],
-       rating: { type: Number, max: 5, default: 0}
-    }, 
-    { timestamps: true }
+  {
+    full_name: { type: String, required: true },
+    bio: { type: String, required: true },
+    email: { type: String, required: true },
+    phone_number: { type: String, required: true },
+    picture: { type: String },
+    availability: [
+      {
+        status: { type: Boolean, default: false },
+        date: String,
+        time: [String],
+      },
+    ],
+    no_of_properties: { type: Number },
+    area_of_operation: { type: [String] },
+    reviews: [
+      {
+        id: String,
+        review_msg: String,
+        name_of_user: String,
+      },
+    ],
+    rating: { type: Number, max: 5, default: 0 },
+  },
+  { timestamps: true }
 );
-
 
 const agentModel = mongoose.model("Agents", agentSchema);
 
 export default agentModel;
-
-
-
-
 
 // const availabilitySchema = new mongoose.Schema({
 //     status: Boolean,
@@ -46,7 +40,6 @@ export default agentModel;
 
 // ====> availability: [availabilitySchema],
 
-
 // const reviewSchema = new mongoose.Schema({
 //     id: String,
 //     review_msg: String,
@@ -54,4 +47,3 @@ export default agentModel;
 // })
 
 // =========> reviews: [reviewSchema],
-
