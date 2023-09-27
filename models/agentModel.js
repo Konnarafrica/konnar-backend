6f1197bc51ab25274b98bf99d45eb2336e53c26c
Eneh Kelvin Chukwuemeka <kelvineneh4@gmail.com>
2023-09-27 08:55:45 +0100

    added another check; made the email anumber unique

--- models/agentModel.js
+++ models/agentModel.js
@@ -4,8 +4,8 @@ const agentSchema = mongoose.Schema(
   {
     full_name: { type: String, required: true },
     bio: { type: String, required: true },
-    email: { type: String, required: true },
-    phone_number: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    phone_number: { type: String, required: true, unique: true },
     picture: { type: String },
     availability: [
       {
